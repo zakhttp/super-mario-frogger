f7b2713d87a6f96a5ec9f86dc4272f15e75c0eb4
zakhttp <zakhttp@gmail.com>
2017-03-26 15:06:09 +0400

    create config object

---
+++ src/js/config.js
@@ -0,0 +1,40 @@
+/**
+ * @description Game         configuration object
+ * @return {object}  config  Object containing the game settings
+ */
+var config = (function () {
+    'use strict';
+    var config = {
+        bounds: {
+            top: 121,
+            right: 780, //808-28
+            bottom: 625,
+            left: 0
+        },
+        rowHeight: 101,
+        colWidth: 101,
+        numRows: 8,
+        numCols: 8,
+        colImages: [
+            'images/plain-sky-block.png',
+            'images/bridge-block.png',
+            'images/bridge-block.png',
+            'images/bridge-block.png',
+            'images/bridge-block.png',
+            'images/bridge-block.png',
+            'images/ground-block.png',
+            'images/plain-ground-block.png'
+        ],
+        playerStep: {
+            x: 25,
+            y: 101
+        },
+        /* Enemies speed ratio to fine-tune the speed*/
+        speedRatio: 10,
+        collisionTolerance: {
+            x: 30,
+            y: 30,
+        }
+    };
+    return config;
+})();
