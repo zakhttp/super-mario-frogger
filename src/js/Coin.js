9c7d5fbe3156164bde6111f0f26f4330aa21af5a
zakhttp <zakhttp@gmail.com>
2017-03-28 20:27:54 +0400

    fix jsdocs errors in the Coin class

--- src/js/Coin.js
+++ src/js/Coin.js
@@ -1,19 +1,22 @@
 'use strict';
 
 /**
- * @description        Collectibles the player should collect
- * @param {number}  x  Horizonal position
- * @param {Number}  y  Vertical position
+ * @description                Collectibles the player should collect
+ * @param {number}  x          Horizonal position
+ * @param {Number}  y          Vertical position
+ * @param {[type]}  colWidth   Width of each column of the game board
+ * @param {number}  rowHeight  Height of each row of the game board
  */
-var Coin = function(x, y) {
+var Coin = function(x, y, colWidth, rowHeight) {
     this.x = (x * colWidth) + (0.5 * colWidth) - 10;
     this.y = (y * rowHeight) + (0.5 * rowHeight) + 73;
     this.sprite = 'images/big-coin.png';
 };
 
 /**
- * @description Draw a coin on the gameboard
+ * @description               Draw a coin on the gameboard
+ * @param  {object}  context  Context to use to draw the coin
  */
-Coin.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+Coin.prototype.render = function(context) {
+    context.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
