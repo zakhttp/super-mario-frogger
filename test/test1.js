e5d05543410fff4a9720ad8c953e477aedcf497b
zakhttp <zakhttp@gmail.com>
2017-03-29 14:31:50 +0400

    rename the test file

--- test/test1.js
+++
@@ -1,8 +0,0 @@
-describe('first test', function() {
-    it('should be true', function() {
-        rowHeight.should.equal(101);
-    });
-    it('should be an object', function() {
-        rowHeight.should.equal(101);
-    });
-});
